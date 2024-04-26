4d34537edca572f94853238a82d37db201c79b1e
Sachin Nimbal <87244512+sachinnimbal@users.noreply.github.com>
2024-04-26 23:23:40 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,86 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const typedElement = document.querySelector(".typed");
+  if (typedElement) {
+    let typedStrings = typedElement.getAttribute("data-typed-items");
+    typedStrings = typedStrings.split(",");
+    new Typed(".typed", {
+      strings: typedStrings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000,
+    });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var buttons = document.querySelectorAll(".button");
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      button.classList.toggle("active");
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .querySelector(".about-btn a")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      const href = this.getAttribute("href");
+
+      if ("scrollBehavior" in document.documentElement.style) {
+        window.scroll({
+          top: document.querySelector(href).offsetTop,
+          behavior: "smooth",
+        });
+      } else {
+        location.href = href;
+      }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const skillsSection = document.querySelector("#skills");
+  const circles = document.querySelectorAll(".progress-ring__circle");
+  const skills = [65, 90, 80, 55, 80, 85, 85, 60];
+
+  circles.forEach((circle, index) => {
+    const radius = circle.r.baseVal.value;
+    const circumference = radius * 2 * Math.PI;
+    circle.style.strokeDasharray = `${circumference} ${circumference}`;
+    if (window.innerWidth < 768) {
+      const offset = circumference - (skills[index] / 100) * circumference;
+      circle.style.strokeDashoffset = offset;
+    } else {
+      circle.style.strokeDashoffset = circumference;
+    }
+  });
+
+  if (window.innerWidth >= 768) {
+    const animateSkills = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          circles.forEach((circle, index) => {
+            setTimeout(() => {
+              const radius = circle.r.baseVal.value;
+              const circumference = radius * 2 * Math.PI;
+              const offset =
+                circumference - (skills[index] / 100) * circumference;
+              circle.style.transition = "stroke-dashoffset 2s ease-out";
+              circle.style.strokeDashoffset = offset;
+            }, index * 250);
+          });
+          observer.unobserve(entry.target);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(animateSkills, {
+      root: null,
+      threshold: 0.5,
+    });
+
+    observer.observe(skillsSection);
+  }
+});

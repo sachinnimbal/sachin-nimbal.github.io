0c7ac680dd67377e0883a4413884051982b4f97e
Sachin Nimbal <87244512+sachinnimbal@users.noreply.github.com>
2024-05-16 10:30:46 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,3 +110,20 @@ document.addEventListener("DOMContentLoaded", function () {
     observer.observe(skillsSection);
   }
 });
+
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("backToTopBtn").style.display = "block";
+    } else {
+        document.getElementById("backToTopBtn").style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+}
